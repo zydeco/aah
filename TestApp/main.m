351995a7cb4a90a33bba04c347b574c1d972bbda
Jesús A. Álvarez <zydeco@namedfork.net>
2019-07-06 14:27:44 +0200

    add TestApp target

---
+++ TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Jesús A. Álvarez on 06/07/2019.
+//  Copyright © 2019 namedfork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, I'm a test app");
+        return 3;
+    }
+}
