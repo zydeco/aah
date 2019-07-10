cd43641f5cc85c41ba43bdb4e20e2dd8eb0b8301
Jesús A. Álvarez <zydeco@namedfork.net>
2019-07-10 18:49:49 +0200

    use correct method signature for blocks

---
+++ Sources/shims/blocks.h
@@ -0,0 +1,19 @@
+#include <Block.h>
+
+const char * _Block_signature(void *aBlock);
+
+struct Block_descriptor {
+    unsigned long int reserved;
+    unsigned long int size;
+    void (*copy)(void *dst, void *src);
+    void (*dispose)(void *);
+};
+
+struct Block_layout {
+    void *isa;
+    volatile int32_t flags; // contains ref count
+    int32_t reserved;
+    void (*invoke)(void *, ...);
+    struct Block_descriptor *descriptor;
+    // imported variables
+};
--- Sources/shims/libdispatch.c
+++ Sources/shims/libdispatch.c
@@ -7,22 +7,15 @@
 //
 
 #import "aah.h"
-
-struct Block_layout {
-    void *isa;
-    volatile int32_t flags; // contains ref count
-    int32_t reserved;
-    void (*invoke)(void *, ...);
-    struct Block_descriptor_1 *descriptor;
-    // imported variables
-};
+#import "blocks.h"
 
 // void function(void * arg, dispatch_block_t block);
 // dispatch_once, dispatch_async
 WRAP_EMULATED_TO_NATIVE(dispatch_block_1) {
     struct Block_layout *block = *(void**)avalues[1];
-    printf("caching cif for block %p\n", block->invoke);
-    cif_cache_add(block->invoke, "v", "(block)");
+    const char *signature = _Block_signature(block) ?: "v?";
+    printf("caching cif for block %p (%s)\n", block->invoke, signature);
+    cif_cache_add(block->invoke, signature, "(block)");
 }
 
 WRAP_EMULATED_TO_NATIVE(dispatch_once_f) {
