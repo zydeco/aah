5593d4eb79e8c85a69e929ae504c8a6343f0dc8c
Jesús A. Álvarez <zydeco@namedfork.net>
2019-07-10 21:30:47 +0200

    pthread glue

--- Sources/marzipan_glue.m
+++ Sources/marzipan_glue.m
@@ -1,20 +1,31 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 #import <dlfcn.h>
-
-
-int dyld_get_active_platform(void);
-
-int my_dyld_get_active_platform() {
-    return 6;
-}
+#import <pthread.h>
+#import "aah.h"
 
 void *my_Block_copy(const void *arg);
+
+int aah_pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg) {
+    printf("caching cif for pthread start routine %p\n", start_routine);
+    cif_cache_add(start_routine, "??", "(start_routine for a pthread)");
+    return pthread_create(thread, attr, start_routine, arg);
+};
+
+int pthread_key_create(pthread_key_t *key, void (*destructor)(void *));
+int aah_pthread_key_create(pthread_key_t *key, void (*destructor)(void *)) {
+    if (destructor != NULL) {
+        printf("caching cif for pthread tsd destructor %p\n", destructor);
+        cif_cache_add(destructor, "v?", "(destructor for thread-specific-data)");
+    }
+    return pthread_key_create(key, destructor);
+}
 
 typedef struct interpose_s { void *new_func; void *orig_func; } interpose_t;
 static const interpose_t interposing_functions[] __attribute__ ((used, section("__DATA, __interpose"))) = {
     { (void*) my_Block_copy, (void*)_Block_copy},
-    //{ (void *)my_dyld_get_active_platform, (void *)dyld_get_active_platform }
+    { (void*) aah_pthread_create, (void*)pthread_create},
+    { (void*) aah_pthread_key_create, (void*)pthread_key_create},
 };
 
 @implementation NSBundle (Marzipan)
