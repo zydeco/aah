907eb831bad975c23a404b27a2275578d4b129c0
Jesús A. Álvarez <zydeco@namedfork.net>
2019-07-29 18:49:56 +0200

    update test app

--- TestApp/main.m
+++ TestApp/main.m
@@ -10,7 +10,16 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        NSLog(@"Hello, I'm a test app");
-        return 3;
+        //NSLog(@"Hello, I'm a test app %d %d %g", 1, 2, 3.0);
+        NSDictionary *dictionary = [[NSDictionary alloc] initWithObjectsAndKeys:@"object", @"key", @"object", @"key2", nil];
+        NSLog(@"The dictionary is %@", dictionary);
+        
+        NSString *string = @"This is a string";
+        NSString *substring = [string substringWithRange:NSMakeRange(5, 2)];
+        NSLog(@"The substring is “%@”", substring);
+        
+        NSRange range = [string rangeOfString:@"is"];
+        NSLog(@"The range is {%d, %d}", (int)range.location, (int)range.length);
+        return 4;
     }
 }
