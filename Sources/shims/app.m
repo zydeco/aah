7bbb2a79a9c255f4a1ec8a8c39e1b7f28ce52434
Jesús A. Álvarez <zydeco@namedfork.net>
2019-08-17 15:01:04 +0200

    add app.m

---
+++ Sources/shims/app.m
@@ -0,0 +1,13 @@
+//
+//  app.m
+//  aah
+//
+//  Created by Jesús A. Álvarez on 2019-07-07.
+//  Copyright © 2019 namedfork. All rights reserved.
+//
+
+#import "aah.h"
+#import "blocks.h"
+#import <Foundation/Foundation.h>
+
+// hooks and stuff for current app...
