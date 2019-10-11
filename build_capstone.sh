ecab6a96c107d4515e0ec23a3bf5c91556c07d20
Jesús A. Álvarez <zydeco@namedfork.net>
2019-10-11 19:25:19 +0200

    use capstone next branch

--- build_capstone.sh
+++ build_capstone.sh
@@ -11,11 +11,11 @@ fi
 # mac build
 if [ ! -f "../lib/libcapstone-aah.a" ]; then
     make clean
-    CAPSTONE_ARCHS="aarch64" CAPSTONE_SHARED=no CAPSTONE_STATIC=yes ./make.sh mac-universal-no && mv libcapstone.a ../lib/libcapstone-aah.a
+    CAPSTONE_ARCHS="aarch64" CAPSTONE_SHARED=no CAPSTONE_STATIC=yes CAPSTONE_BUILD_CORE_ONLY=yes ./make.sh mac-universal-no && mv libcapstone.a ../lib/libcapstone-aah.a
 fi
 
 # iOS build
 if [ ! -f "../lib/libcapstone-ios.a" ]; then
     make clean
-    CAPSTONE_ARCHS="aarch64 x86" CAPSTONE_SHARED=no CAPSTONE_STATIC=yes ./make.sh ios arm64 && mv libcapstone.a ../lib/libcapstone-ios.a
+    CAPSTONE_ARCHS="aarch64 x86" CAPSTONE_SHARED=no CAPSTONE_STATIC=yes CAPSTONE_BUILD_CORE_ONLY=yes ./make.sh ios arm64 && mv libcapstone.a ../lib/libcapstone-ios.a
 fi
