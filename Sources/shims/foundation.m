e9d644a9778942a944ae79a31fb762feb8666493
Jesús A. Álvarez <zydeco@namedfork.net>
2019-10-13 21:28:57 +0200

    generic obj-c shim for nil-terminated varargs

--- Sources/shims/foundation.m
+++ Sources/shims/foundation.m
@@ -12,20 +12,33 @@ size_t NumberOfNonNullVarArgs(void ** stack) {
     return arg;
 }
 
+static inline int NumberOfArgsFromSelector(SEL cmd) {
+    int count = 0;
+    for(const char *c = (void*)cmd; *c; c++) {
+        if (*c == ':') {
+            count++;
+        }
+    }
+    return count;
+}
+
 // obj-c call with nil-terminated list of objects:
 // initWithObjectsAndKeys:firstObject,...
+// initWithSomething:firstArg andListOfWhatever:secondArg, ...
 
 SHIMDEF(nilTerminatedListOfObjects) {
-    size_t args = NumberOfNonNullVarArgs((void**)ctx->sp);
-    char *argEncoding = alloca(3 + args + 3);
-    memset(argEncoding, '@', 3 + args + 2);
+    size_t varArgs = NumberOfNonNullVarArgs((void**)ctx->sp);
+    int fixedArgs = NumberOfArgsFromSelector((SEL)ctx->arm64_call_context->x[1]);
+    size_t totalArgs = fixedArgs + varArgs + 1;
+    char *argEncoding = alloca(3 + totalArgs + 1);
+    memset(argEncoding, '@', 3 + totalArgs);
     argEncoding[2] = ':';
-    argEncoding[3 + args + 2] = '\0';
+    argEncoding[3 + totalArgs] = '\0';
     printf("calling with arg encoding %s\n", argEncoding);
     // construct call
     ffi_cif cif_native;
     ffi_cif_arm64 cif_arm64;
-    prep_cifs(&cif_native, &cif_arm64, argEncoding, 3);
+    prep_cifs(&cif_native, &cif_arm64, argEncoding, 2 + fixedArgs);
     ctx->cif_native = &cif_native;
     ctx->cif_arm64 = &cif_arm64;
     ctx->before = ctx->after = NULL;
